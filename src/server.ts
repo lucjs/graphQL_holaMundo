f23864a6c6fbfb0c47f978fce39437320e2108f6
lucjs <lucaspercello@gmail.com>
2020-09-26 09:58:49 -0300

    src/server.js

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,16 @@
-console.log('Hola mundo en GRAPHQL...')
+import express from 'express';
+import compression from "compression";
+import cors from "cors";
+const app_express = express();
+const PORT = 8000;
+
+app_express.use('*', cors());
+app_express.use(compression());
+
+app_express.use('/', (re, res) => {
+    res.send('API corriendo...');
+});
+
+app_express.listen({ port: PORT }, () => {
+    console.log("Server on port 8000 -> url http://localhost:8000");
+});
