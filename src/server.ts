76948bea6b503aa94a912bd1cd5466f563025832
lucjs <lucaspercello@gmail.com>
2020-09-26 10:21:20 -0300

    src/server.js (GRAPHQL)

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,39 @@
 import express from 'express';
 import compression from "compression";
 import cors from "cors";
+import { IResolvers, makeExecutableSchema } from 'graphql-tools';
+import { GraphQLSchema } from 'graphql';
+import { graphqlHTTP } from 'express-graphql';
 const app_express = express();
 const PORT = 8000;
+const typeDefs = ` type Query {
+    hola: String! 
+    holaNombre(nombre: String!): String!
+} `
+const resolvers : IResolvers = {
+    Query : {
+        hola(): string {
+            return '¿ Hola como estas ?';
+        },
+        holaNombre(__:void, { nombre }): string {
+            return `¿ Hola como estas ${nombre} ?`;
+        }        
+    }
+}
+const schema: GraphQLSchema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+})
 
 app_express.use('*', cors());
 app_express.use(compression());
 
-app_express.use('/', (re, res) => {
-    res.send('API corriendo...');
-});
+app_express.use('/', graphqlHTTP({
+    schema,
+    graphiql: true
+}));
+
 
 app_express.listen({ port: PORT }, () => {
-    console.log("Server on port 8000 -> url http://localhost:8000");
+    console.log("Server on port 8000 -> url http://localhost:8000/graphql");
 });
